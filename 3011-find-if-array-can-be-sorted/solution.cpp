0b97ba055e2670d4f231157d0ad9282e9518ad65
Zedespook <26501287+Zedespook@users.noreply.github.com>
2024-12-04 02:18:08 +0100

    style(format): Reformat solution file for 3011 with Clang

--- 3011-find-if-array-can-be-sorted/solution.cpp
+++ 3011-find-if-array-can-be-sorted/solution.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    bool canSortArray(vector<int>& nums) {
-        const int n = nums.size();
-        if (n <= 1) return true;
-        
-        int left = 0;
-        while (left < n) {
-            const int currentBits = __builtin_popcount(nums[left]);
-            int right = left + 1;
-            
-            while (right < n && __builtin_popcount(nums[right]) == currentBits) {
-                right++;
-            }
-            
-            if (left > 0) {
-                const int prevBits = __builtin_popcount(nums[left - 1]);
-                if (prevBits != currentBits) {
-                    const int prevMax = *ranges::max_element(nums.begin(), nums.begin() + left);
-                    const int curMin = *ranges::min_element(nums.begin() + left, nums.begin() + right);
-                    if (prevMax > curMin) return false;
-                }
-            }
-            
-            ranges::sort(nums.begin() + left, nums.begin() + right);
-            left = right;
+  bool canSortArray(vector<int> &nums) {
+    const int n = nums.size();
+    if (n <= 1)
+      return true;
+
+    int left = 0;
+    while (left < n) {
+      const int currentBits = __builtin_popcount(nums[left]);
+      int right = left + 1;
+
+      while (right < n && __builtin_popcount(nums[right]) == currentBits) {
+        right++;
+      }
+
+      if (left > 0) {
+        const int prevBits = __builtin_popcount(nums[left - 1]);
+        if (prevBits != currentBits) {
+          const int prevMax =
+              *ranges::max_element(nums.begin(), nums.begin() + left);
+          const int curMin =
+              *ranges::min_element(nums.begin() + left, nums.begin() + right);
+          if (prevMax > curMin)
+            return false;
         }
-        
-        return true;
+      }
+
+      ranges::sort(nums.begin() + left, nums.begin() + right);
+      left = right;
     }
+
+    return true;
+  }
 };
